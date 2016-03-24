58df66d84cdc16d8891ea582faca51cf7b2eab18
Pavlo Sobchuk <pavlo.sobchuk@gmail.com>
2016-03-24 21:16:12 +0200

    Render posts on index page. Add more index page content

--- routes/views/index.js
+++ routes/views/index.js
@@ -8,6 +8,30 @@ exports = module.exports = function(req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
+	locals.data = {
+		posts: []
+	};
+	
+	// Load the posts
+	view.on('init', function(next) {
+		
+		var q = keystone.list('Post').paginate({
+				page: req.query.page || 1,
+				perPage: 10,
+				maxPages: 10,
+				filters: {
+					'state': 'published'
+				}
+			})
+			.sort('-publishedDate')
+			.populate('author categories');
+		
+		q.exec(function(err, results) {
+			locals.data.posts = results;
+			next(err);
+		});
+		
+	});
 	
 	// Render the view
 	view.render('index');
